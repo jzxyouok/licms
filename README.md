5e3007b9fd87d8e7822f425e40f6a0e0c4f55acd
Liyiren <378907426@qq.com>
2016-03-01 09:15:34 +0800

    ignore文件

---
+++ README.md
@@ -0,0 +1,14 @@
+## README
+Licms 是基于ThinkCMF 的内容管理系统，在其框架至上进行二次开发。
+
+## UPDATE
+v1.0.0
+* 修改后台侧边栏Font-Awesone效果
+
+
+## INSTALL
+安装请执行http://yourdomain/install/index.php
+
+安装完成后请删除或改名install/index.php
+
+感谢ThinkCMF团队。
---
+++ data/conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * 配置文件
+ */
+return array(
+    'DB_TYPE' => 'mysql',
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'thinkcmf',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'root',
+    'DB_PORT' => '3306',
+    'DB_PREFIX' => 'cmf_',
+    //密钥
+    "AUTHCODE" => 'cT6ofd65hcbXnFJypt',
+    //cookies
+    "COOKIE_PREFIX" => 'L4kHl4_',
+);
