b128b3dce0a859aee434bbd8def2659b2e398cf6
Liyiren <378907426@qq.com>
2016-03-02 17:07:46 +0800

    修改样式及内容

--- application/Admin/Controller/MainController.class.php
+++ application/Admin/Controller/MainController.class.php
@@ -11,14 +11,14 @@ class MainController extends AdminbaseController {
     	
     	//server infomaions
     	$info = array(
-    			L('OPERATING_SYSTEM') => PHP_OS,
-    			L('OPERATING_ENVIRONMENT') => $_SERVER["SERVER_SOFTWARE"],
+    			// L('OPERATING_SYSTEM') => PHP_OS,
+    			// L('OPERATING_ENVIRONMENT') => $_SERVER["SERVER_SOFTWARE"],
     			L('PHP_RUN_MODE') => php_sapi_name(),
     			L('MYSQL_VERSION') =>$mysql,
-    			L('PROGRAM_VERSION') => SIMPLEWIND_CMF_VERSION . "&nbsp;&nbsp;&nbsp; [<a href='http://www.thinkcmf.com' target='_blank'>ThinkCMF</a>]",
+    			L('PROGRAM_VERSION') => "v1.0",
     			L('UPLOAD_MAX_FILESIZE') => ini_get('upload_max_filesize'),
     			L('MAX_EXECUTION_TIME') => ini_get('max_execution_time') . "s",
-    			L('DISK_FREE_SPACE') => round((@disk_free_space(".") / (1024 * 1024)), 2) . 'M',
+    			// L('DISK_FREE_SPACE') => round((@disk_free_space(".") / (1024 * 1024)), 2) . 'M',
     	);
     	$this->assign('server_info', $info);
     	$this->display();
--- application/Admin/Lang/zh-cn.php
+++ application/Admin/Lang/zh-cn.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-		'ADMIN_CENTER' => '后台管理中心',
-		'WELCOME_USER' => '欢迎, {$username}',
-		'WEBSITE_HOME_PAGE' => '网站首页'
+		'ADMIN_CENTER' => '内容管理系统平台',
+		'WELCOME_USER' => '欢迎你, {$username}',
+		'WEBSITE_HOME_PAGE' => '前台首页'
 		
 );
